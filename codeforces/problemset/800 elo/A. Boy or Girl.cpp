4e1eeb5cd94e8084b818469523e08384b192d34e
Teepat17 <asiaasdf719@gmail.com>
2025-07-10 10:34:35 +0700

    t

--- codeforces/problemset/800 elo/A. Boy or Girl.cpp
+++ codeforces/problemset/800 elo/A. Boy or Girl.cpp
@@ -5,6 +5,12 @@ using namespace std;
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    string name;
+    cin >> name;
+
+    set <char> user(name.begin(),name.end());
+
+    cout << (user.size()%2 == 0 ? "CHAT WITH HER!" : "IGNORE HIM!");
 
     return 0;
 }
--- codeforces/problemset/800 elo/A. Word Capitalization.cpp
+++ codeforces/problemset/800 elo/A. Word Capitalization.cpp
@@ -5,6 +5,13 @@ using namespace std;
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    string s;
+    cin >> s;
 
+    if(islower(s[0])){
+        s[0] = (char)toupper(s[0]);
+    }
+
+    cout << s;
     return 0;
 }
